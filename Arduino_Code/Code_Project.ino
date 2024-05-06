58e13995c7ffce2ea0e3b593a27c68cd4b680884
Sasa-Indeed <ahmed382002@gmail.com>
2024-05-06 17:15:39 +0100

    Finished the logic runs well needs testing

--- Arduino_Code/Code_Project.ino
+++ Arduino_Code/Code_Project.ino
@@ -1,6 +1,7 @@
 #include <Servo.h>
 #include "LiquidCrystal_I2C.h"
 
+//Gate controls
 Servo servoMotor;  
 
 #define IR_SENSOR_IN_GATE_PIN         4
@@ -9,32 +10,43 @@ Servo servoMotor;
 
 
 //Parking Slots
-#define MAXSLOTSNUMBER                       4
-#define PARKINGSLOT1                         10
-#define PARKINGSLOT2                         11
-#define PARKINGSLOT3                         12
-#define PARKINGSLOTWASHING4                  13
+#define MAX_SLOTS_NUMBER                       4
+#define WASHING_SLOTS_NUMBER                   1
+#define PARKING_SLOT1                          10
+#define PARKING_SLOT2                          11
+#define PARKING_SLOT3                          12
+#define PARKING_SLOT_WASHING4                  13
 
-/*
-  1-Check availble(slotsCheck) to see availble spaces
-  2-(fees) return fees 50 or 200
-  3-(openDoor) return yes
-  4-(checkParking) send the path  
-  5-(checkWashing) send path 
+//Parking Slots Directions
+String slots[4] = {"1,Straight Until Point 1,Right", "1,Straight Until Point 1,Straight Until Point 2,Left", 
+                   "1,Straight Until Point 1,Straight Until Point 2,Straight Until Point 3,Right,Straight Until Point 4,Left",
+                   "1,Straight Until Point 1,Straight Until Point 2,Straight Until Point 3,Left,Straight Until Point 4,Right"};
+String NotAvailable = "0, No Free Available Spaces";
+
+/* Commands 
+  1-Check availble(slotsCheck) to see available spaces
+  2-(openDoor) return yes for exiting
+  3-(checkParking) send the path or not available  
+  4-(checkWashing) send path or not available 
 */
+#define CMD_CHECK_AVAILABLE_SLOTS                     "slotsCheck"
+#define CMD_OPEN_DOOR                                 "openDoor"
+#define CMD_CHECK_AVAILABLE_PARKING_SLOTS             "checkParking"
+#define CMD_CHECK_AVAILABLE_WASHING_SLOTS             "checkWashing"
 
 //LCD
 LiquidCrystal_I2C lcd(0x27,  16, 2);
 
 
 uint8_t parkingSpaces[4] = {0,0,0,0};
-
-volatile uint8_t availablePlaces = 4;
+String availability = "AN";
 
 void IR_SensorSecureCloseGate(uint8_t dir);
 void ServorCloseGate(void);
 void ServorOpenGate(void);
 
+/* Reads the parking slots availablility */
+void GetParkingReadings(void);
 /* This function returns the closet free space
  * the first parameter is to return the number of available spaces*/
 String CheckAvailableSpaces(uint8_t * free_spaces);
@@ -42,7 +54,9 @@ String CheckAvailableSpaces(uint8_t * free_spaces);
 /**/
 void LcdPrintString(String first_line, String second_line);
 
-void OpenGate(uint8_t direction);
+String OpenGate(uint8_t direction);
+String CheckParkingSpaces(bool* available, uint8_t * slotNumber);
+String CheckWashingSpaces(bool* available, uint8_t * slotNumber);
 
 void setup() {
   // put your setup code here, to run once:
@@ -50,10 +64,10 @@ void setup() {
   pinMode(IR_SENSOR_OUT_GATE_PIN, INPUT);
 
   //Parking Slots congfigurations
-  pinMode(PARKINGSLOT1, INPUT_PULLUP);
-  pinMode(PARKINGSLOT2, INPUT_PULLUP);
-  pinMode(PARKINGSLOT3, INPUT_PULLUP);
-  pinMode(PARKINGSLOTWASHING4, INPUT_PULLUP);
+  pinMode(PARKING_SLOT1, INPUT_PULLUP);
+  pinMode(PARKING_SLOT2, INPUT_PULLUP);
+  pinMode(PARKING_SLOT3, INPUT_PULLUP);
+  pinMode(PARKING_SLOT_WASHING4, INPUT_PULLUP);
 
   //LCD
   lcd.init();
@@ -70,14 +84,45 @@ void setup() {
 
 void loop() {
   // put your main code here, to run repeatedly:
+  String state;
+  String data; 
+  uint8_t availableSpaces;
+  bool isSpaceAvailable;
 
-  Serial.println(digitalRead(PARKINGSLOT1));
+  //Checking if bluetooth is sending data
+  if(Serial.available() > 0){
+    state = Serial.readString();
 
-  if(digitalRead(PARKINGSLOT1) == HIGH){
-    OpenGate(IR_SENSOR_OUT_GATE_PIN);
+    if(state.substring(0,10) == CMD_CHECK_AVAILABLE_SLOTS){
+        data = CheckAvailableSpaces(&availableSpaces);
+        Serial.println(data);
+        LcdPrintString("1:" + String(availability[parkingSpaces[0]]) + 
+                       " 2:" + String(availability[parkingSpaces[1]]) +
+                       " 3:" + String(availability[parkingSpaces[2]]) +
+                       " 4:" + String(availability[parkingSpaces[3]]) , 
+                       "Available ST: " + String(availableSpaces));
+    }else if(state.substring(0,8) == CMD_OPEN_DOOR){
+        OpenGate(IR_SENSOR_OUT_GATE_PIN);
+    }else if(state.substring(0,12) == CMD_CHECK_AVAILABLE_PARKING_SLOTS){
+        data = CheckParkingSpaces(&isSpaceAvailable, &availableSpaces);
+        if(isSpaceAvailable){
+          Serial.println(data); //Direction
+          LcdPrintString("Slot Number P" + String(availableSpaces) + " A", "Directions  Sent");
+          OpenGate(IR_SENSOR_IN_GATE_PIN);
+        }else{
+          LcdPrintString(" Sorry No Free  ", "Available Spaces");
+        }
+    }else if(state.substring(0,12) == CMD_CHECK_AVAILABLE_WASHING_SLOTS){
+        data = CheckWashingSpaces(&isSpaceAvailable, &availableSpaces);
+        if(isSpaceAvailable){
+          Serial.println(data); //Direction
+          LcdPrintString("Slot Number W" + String(availableSpaces) + " A", "Directions  Sent");
+          OpenGate(IR_SENSOR_IN_GATE_PIN);
+        }else{
+          LcdPrintString(" Sorry No Free  ", "Available Spaces");
+        }
+    }
   }
-
-  delay(100);
 }
 
 void IR_SensorSecureCloseGate(uint8_t dir){
@@ -101,16 +146,19 @@ void ServorOpenGate(void){
   servoMotor.write(0);
 }
 
+void GetParkingReadings(void){
+  parkingSpaces[0] = digitalRead(PARKING_SLOT1);
+  parkingSpaces[1] = digitalRead(PARKING_SLOT2);
+  parkingSpaces[2] = digitalRead(PARKING_SLOT3);
+  parkingSpaces[3] = digitalRead(PARKING_SLOT_WASHING4);
+}
+
 String CheckAvailableSpaces(uint8_t * free_spaces){
-  *free_spaces = MAXSLOTSNUMBER;
+  *free_spaces = MAX_SLOTS_NUMBER;
 
-  parkingSpaces[0] = digitalRead(PARKINGSLOT1);
-  parkingSpaces[1] = digitalRead(PARKINGSLOT2);
+  GetParkingReadings();
 
-  parkingSpaces[2] = digitalRead(PARKINGSLOT3);
-  parkingSpaces[3] = digitalRead(PARKINGSLOTWASHING4);
-
-  for (uint8_t i = 0; i < MAXSLOTSNUMBER; i++) {
+  for (uint8_t i = 0; i < MAX_SLOTS_NUMBER; i++) {
     *free_spaces -= parkingSpaces[i];
   }
 
@@ -124,17 +172,42 @@ void LcdPrintString(String first_line, String second_line){
   lcd.print(second_line);
 }
 
-void OpenGate(uint8_t direction){
+String OpenGate(uint8_t direction){
     servoMotor.write(0);
     IR_SensorSecureCloseGate(direction);
+    return "2,Door has opened";
 }
 
-String Fees(void){
-  uint8_t randNumber = random(50, 200);
-  if(randNumber < 125){
-    return "2,50";
-  }else{
-    return "2,200";
+String CheckParkingSpaces(bool* available, uint8_t * slotNumber){
+  GetParkingReadings();
+
+  for (uint8_t i = 0; i < MAX_SLOTS_NUMBER - WASHING_SLOTS_NUMBER; i++) {
+    if(!parkingSpaces[i]){
+      *available = true;
+      *slotNumber = i + 1;
+      return "3," + slots[i];
+    }
   }
+
+  *slotNumber = 0;
+  *available = false;
+  return "3," + NotAvailable;
 }
+
+String CheckWashingSpaces(bool* available, uint8_t * slotNumber){
+  GetParkingReadings();
+
+  for (uint8_t i = MAX_SLOTS_NUMBER - WASHING_SLOTS_NUMBER; i < MAX_SLOTS_NUMBER; i++) {
+    if(!parkingSpaces[i]){
+      *available = true;
+      *slotNumber = i + 1;
+      return "4," + slots[i];
+    }
+  }
+
+  *available = false;
+  *slotNumber = 0;
+  return "4," + NotAvailable;
+}
+
 
